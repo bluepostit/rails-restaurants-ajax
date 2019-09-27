ac46f22e1b85aa1c149142734e5b3a97b5e03233
bluepostit <32864499+bluepostit@users.noreply.github.com>
2019-09-27 09:08:28 +0200

    implement create review, views + ctlr

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :reviews, only: :create
+  end
 end
